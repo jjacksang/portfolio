47f755a0488c8bff3ec7a78566f22b18bd303e2e
jacksang <jswoo1995@gmail.com>
2025-10-11 17:35:16 +0900

    fix: postcss install

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,6 @@ export default function Home() {
   return (
     <>
       <QuickButton />
-
-      <div className="flex justify-between bg-red-500 p-2 text-center">
-        test
-      </div>
     </>
   );
 }
---
+++ app/styles/theme.ts
@@ -0,0 +1,17 @@
+export const size = {
+  tablet: '640px',
+  laptop: '1200px',
+  desktop: '1800px',
+};
+
+export const theme = {
+  colors: {
+    primary: '#121212',
+    secondary: '#FACC15',
+    background: '#F9FAFB',
+    text: '0.8rem',
+  },
+  size,
+};
+
+export type ThemeType = typeof theme;
